3eb1234b565bc5e1870463d4305f65473520a668
Annes Bani Al Doumi <annesdoumi2@gmail.com>
2024-10-08 01:33:23 +0200

    Prompt bereinigt

--- src/components/UploadComponent.vue
+++ src/components/UploadComponent.vue
@@ -245,7 +245,7 @@ export default {
 
     // Methoden zum Import in Kalender (Google/Apple)
     importGoogleCalendar() {
-      const prompt = "Extract the following data into a clean CSV format with the columns: Subject, Start Date, Start Time, End Date, End Time, Description. Remove any headers, and only include the actual schedule data (Subject, Start Date, Start Time, End Date, End Time, Description). Do not include any additional commentary or information. If there is too much loss of data , Please extract the following format from the input: Date, Start Time, End Time, Description. Ignore any other text or information that is not part of the schedule and also if there are given hours for a date which overlap in one date, only take the earliest time for start time and the latest given time for end time of this date";
+      const prompt = "Extract the following data into a clean CSV format with the columns: Subject, Start Date, Start Time, End Date, End Time, Description. Remove any headers, and only include the actual schedule data (Subject, Start Date, Start Time, End Date, End Time, Description). If there is too much data loss, only take a start date,start time, end date, end time and also think logically if there is one day with more than one start time and end time, take the earliest start time and the latest end time, and put them for this day ";
       this.analyzeFile(prompt);
     },
 
