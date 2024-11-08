971cb857b7c939a3b19824582787450d3e4f7f5b
Annes Bani Al Doumi <annesdoumi2@gmail.com>
2024-11-08 12:08:06 +0100

    Git Online

--- webtechproject/vue.config.js
+++ webtechproject/vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'assets',
+  devServer: {
+    historyApiFallback: true,
+  },
+};
